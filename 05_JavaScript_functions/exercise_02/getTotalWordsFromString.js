1f9be0f2efba14040b58b3e46d50a846a402ace7
Zilong Zhan <zilong.zhan@mataro.epiaedu.cat>
2023-11-14 20:58:54 +0100

    Complete exercises 00 01 02

--- 05_JavaScript_functions/exercise_02/getTotalWordsFromString.js
+++ 05_JavaScript_functions/exercise_02/getTotalWordsFromString.js
@@ -3,3 +3,14 @@
  * input: string (sentence)
  * output: number
  */
+
+/**
+ * Find number of characters in a word
+ * @param {string} word
+ * @returns number of characters of word
+ */
+const getTotalWordsFromString = function (word) {
+  return [...word].length;
+};
+
+export default getTotalWordsFromString;
