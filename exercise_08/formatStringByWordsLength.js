d21caf73ad53891b8343780b70fd0fc6a176ed54
Zilong Zhan <144822890+ZilongZhan@users.noreply.github.com>
2023-11-22 05:01:46 +0100

    Complete challenge_step2 exercises

---
+++ exercise_08/formatStringByWordsLength.js
@@ -0,0 +1,33 @@
+import getTotalWordsFromString from "../exercise_02/getTotalWordsFromString.js"
+
+/**
+ * Write JS function "formatStringByWordsLength" to check number of words contained in given string
+    - if total number of words is greater then 5, then return uppercase string
+    - otherwise, return lower case string
+ * input: string (sentence) 
+ * output: string
+ */
+
+/**
+ * CASE #1
+ * input: "This is a long, long, long sentence."
+ * output: "THIS IS A LONG, LONG, LONG SENTENCE."
+ */
+
+/**
+ * CASE #2 * input: "This is short one."
+ * output: "this is short one."
+ */
+
+/**
+ * Coverts sentence to upper or lower case depending on word count.
+ * @param {string} sentence 
+ * @returns {string}
+ */
+const formatStringByWordsLength = function (sentence) {
+   const wordQuantity = getTotalWordsFromString(sentence);
+   
+   return wordQuantity > 5 ? sentence.toUpperCase() : sentence.toLowerCase();
+}
+
+export default formatStringByWordsLength;
---
+++ exercise_08/index.js
@@ -0,0 +1,8 @@
+//imports
+import formatStringByWordsLength from "./formatStringByWordsLength.js";
+
+const result1 = formatStringByWordsLength("I am so smart.");
+console.log(result1); // i am so smart.
+
+const result2 = formatStringByWordsLength("I am even smarter than Albert Einstein.")
+console.log(result2); // I AM EVEN SMARTER THAN ALBERT EINSTEIN.
